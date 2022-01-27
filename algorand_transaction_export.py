20f19936765af81fab06f7766c500b1ac0eaf592
TeneoPython01 <55600849+TeneoPython01@users.noreply.github.com>
2022-01-26 22:42:47 -0600

    Update algorand_transaction_export.py

--- algorand_transaction_export.py
+++ algorand_transaction_export.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pandas.io.json import json_normalize
 import requests
 from datetime import datetime
+import numpy as np
 
 #enter one or more wallet addresses, then run the code.  CSVs will export to the working directory of the code.
 #the wallets below are some wallet addresses I found by searching google.  they are here to demo the code only.
@@ -9,6 +10,12 @@ my_wallets = [
     'Y76M3MSY6DKBRHBL7C3NNDXGS5IIMQVQVUAB6MP4XEMMGVF2QWNPL226CA',
     'WLWWUUU2HNSYE7L5MY5CH5PMEU2IDC32UMREY363YMJU5JGILTW3WE3UFI'
     ]
+
+#enter how many hours your local time is from UDT
+adjust_hours = -5 #this example would be eastern US time
+
+#NOTE, when the export is complete, the last several columns that start with "z_" are
+#the most commonly needed fields
 
 #loop through the wallets
 for my_wallet in my_wallets:
@@ -45,9 +52,26 @@ for my_wallet in my_wallets:
             looper = 0
             print(str(len(df_full)), ' total txns found in this wallet.')
 
+        df_iteration['z_scanned_wallet'] = my_wallet
         df_full = pd.concat([df_full, df_iteration], sort=True)
 
         iteration = iteration + 1
+
+    #create columns at the end of the data set with the most important data elements
+    df_full['z_excel_datetime_udt'] = ((((df_full['timestamp']/60)/60)/24)+25569) # convert unix format and keep in UDT timezone
+    df_full['z_excel_datetime_local'] = df_full['z_excel_datetime_udt'] + (adjust_hours/24) # adjust the datetime to local time using the adjust_hours variable at the start of the script
+    df_full['z_amt_whole'] = df_full['amount']/1000000.000000 #convert amount to whole units instead of how the API displays millionths without decimals
+    df_full['z_from'] = df_full['from']
+    df_full['z_to'] = df_full['to']
+    try:
+        df_full['z_symbol'] = df_full['unitName']
+    except:
+        pass
+    df_full['z_txid'] = df_full['txid']
+    df_full['z_fee_whole'] = df_full['fee']/1000000.000000
+    df_full['z_type'] = np.where(df_full['z_scanned_wallet']==df_full['from'],'send',np.where(df_full['z_scanned_wallet']==df_full['to'],'receive','other'))
+
+    df_smaller = df_full[df_full.columns[:-7]]
 
     #create variables for date and time in yyyymmdd and hhmmss format to use in the exported CSV filename
     my_date = datetime.now().year*10000+datetime.now().month*100+datetime.now().day
@@ -57,3 +81,4 @@ for my_wallet in my_wallets:
     filename = './algo_export_'+my_wallet[0:6]+'_'+str(my_date)+'_'+str(my_time)+'.csv'
     df_full.to_csv(filename)
     print('export complete: ' + filename + '\n\n')
+
